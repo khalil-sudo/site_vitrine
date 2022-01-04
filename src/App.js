00f9f09e5cbf1a3001fdf31919d640c8fc749add
khalil-sudo <khalilguezguez30@gmail.com>
2022-01-04 13:30:07 +0100

    added new script.js modified App.js for routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="project-1" element={<Portfolio />} />
+      <Route path="contact" element={<Contact />} />
+    </Routes>
+  );
 };
 
 export default App;

961bcce46adc0f660c04a8673298750cea021b77
khalil-sudo <khalilguezguez30@gmail.com>
2022-01-05 00:51:33 +0100

    added DynamicText + main central Home text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import SocialNetwork from "../components/SocialNetwork";
-
+import DynamicText from "../components/DynamicText";
 const Home = () => {
   return (
     <div>
-      <Navigation />
-      <SocialNetwork />
+      <div className="home">
+        <Navigation />
+        <SocialNetwork />
+        <div className="home-main">
+          <div className="main-content">
+            <h1>Mon Site</h1>
+            <h2>
+              <DynamicText />
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

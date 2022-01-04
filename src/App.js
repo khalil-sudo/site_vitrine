28c570a53aff1eb9b1540a22fa3ab8f3c4d3381a
khalil-sudo <khalilguezguez30@gmail.com>
2022-01-04 13:53:15 +0100

    modified porfolio file to projects - added 4

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
-import Portfolio from "./pages/Portfolio";
+import { Project1, Project2, Project3, Project4 } from "./pages/Projects";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="project-1" element={<Portfolio />} />
+      <Route path="project-1" element={<Project1 />} />
+      <Route path="project-2" element={<Project2 />} />
+      <Route path="project-3" element={<Project3 />} />
+      <Route path="project-4" element={<Project4 />} />
       <Route path="contact" element={<Contact />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>

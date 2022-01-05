aef6d93bad2817f113b8eed7e906ca26347dab59
khalil-sudo <khalilguezguez30@gmail.com>
2022-01-05 04:01:24 +0100

    added contact Form and emailing prompt + contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,96 @@
 import React from "react";
+import Navigation from "../components/Navigation";
+import ButtonsBottom from "../components/ButtonsBottom";
+import ContactForm from "../components/ContactForm";
+import SocialNetwork from "../components/SocialNetwork";
+import Logo from "../components/Logo";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Mouse from "../components/Mouse";
+import { motion } from "framer-motion";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const pageTransition = {
+    in: {
+      opacity: 1,
+      x: 0,
+    },
+    out: {
+      opacity: 0,
+      x: 300,
+    },
+  };
+
+  const transition = {
+    ease: [0.03, 0.87, 0.73, 0.9],
+    duration: 0.6,
+  };
+
+  return (
+    <main>
+      <Mouse />
+      <motion.div
+        className="contact"
+        exit="out"
+        animate="in"
+        initial="out"
+        variants={pageTransition}
+        transition={transition}
+      >
+        <Navigation />
+        <Logo />
+        <ContactForm />
+        <div className="contact-infos">
+          <div className="address">
+            <div className="content">
+              <h4>adresse</h4>
+              <p>9 rue des Vergers</p>
+              <p>2017 Bardo</p>
+            </div>
+          </div>
+          <div className="phone">
+            <div className="content">
+              <h4>téléphone</h4>
+              <CopyToClipboard text="71 854 954" className="hover">
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => {
+                    alert("Téléphone copié !");
+                  }}
+                >
+                  71 854 954
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <div className="email">
+            <div className="content">
+              <h4>email</h4>
+              <CopyToClipboard
+                text="MonSiteVitrine.V.1@gmail.com"
+                className="hover"
+              >
+                <p
+                  style={{ cursor: "pointer" }}
+                  className="clipboard"
+                  onClick={() => {
+                    alert("Email copié !");
+                  }}
+                >
+                  MonSiteVitrine.V.1@gmail.com
+                </p>
+              </CopyToClipboard>
+            </div>
+          </div>
+          <SocialNetwork />
+          <div className="credits">
+            <p>Guezguez - 2021</p>
+          </div>
+        </div>
+        <ButtonsBottom left={"/projet-4"} />
+      </motion.div>
+    </main>
+  );
 };
 
 export default Contact;
